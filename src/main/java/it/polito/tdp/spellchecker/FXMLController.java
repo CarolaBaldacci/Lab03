a71221ff4e425fad5eb419b6fd65328c34797cec
HP <HP@LAPTOP-RDR8DTM0.home-life.hub>
2022-11-10 13:04:02 +0100

    parte FXML

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,51 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+    @FXML
+    private Button btnClear;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+    @FXML
+    private Button btnSpell;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private TextArea txtCorreggere;
+
+    @FXML
+    private TextArea txtCorretto;
+
+    @FXML
+    private Label txtErrori;
+
+    @FXML
+    private MenuButton txtLanguage;
+
+    @FXML
+    private Label txtTempo;
+
+    @FXML
+    void doClear(ActionEvent event) {
+
+    	txtCorreggere.clear();
+    	txtCorretto.clear();
+    	txtErrori.setDisable(true);
+    	txtLanguage.disarm();
+    	txtTempo.setDisable(true);
+    	
+    }
+
+    @FXML
+    void doSpell(ActionEvent event) {
 
     }
+
 }
 
 
